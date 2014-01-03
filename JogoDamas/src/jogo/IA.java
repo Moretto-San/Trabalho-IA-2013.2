3353e473ea74f3a9c61ae325ec6ff503b82a1961
moretto-san <rafael.morett@gmail.com>
2014-01-03 19:19:44 -0200

    .

--- JogoDamas/src/jogo/IA.java
+++ JogoDamas/src/jogo/IA.java
@@ -1,5 +1,17 @@
 package jogo;
 
+import java.util.ArrayList;
+
 public class IA {
 
+	
+	public static void MINIMAXDECISION(Tabuleiro tabuleiro){
+		
+	}
+	
+	
+	public static ArrayList<String> getActions(Tabuleiro tabuleiro){
+		
+		return null;
+	}
 }
